9f0f83c52ac65a29b639efec3202a85fba718b3e
Enzu83 <enzu.mc@gmail.com>
2025-08-13 00:18:34 +0200

    remove irrelevant fields in pokedex, create structs to store pokedex json info

--- src/common.rs
+++ src/common.rs
@@ -10,44 +10,31 @@ pub struct Move {
     typ: String,
 }
 
-#[derive(PartialEq)]
-pub enum Type {
-    Normal,
-    Grass,
-    Fire,
-    Water,
-    Fighting,
-    Flying,
-    Poison,
-    Electric,
-    Ground,
-    Psychic,
-    Rock,
-    Ice,
-    Bug,
-    Dragon,
-    Ghost,
-    Dark,
-    Steel,
-    Fairy,
-}
-
 pub struct Stats {
     hp: u32,
-    attack: u32,
-    defense: u32,
-    
+    att: u32,
+    def: u32,
+    spa: u32,
+    spd: u32,
+    spe: u32,
 }
 
 
+// abstract pokemon struct
 pub struct Species {
     id: u32,
-    name: &'static str,
-    types: (Type, Option<Type>)
-    base_stats: 
+    name: String,
+    types: (String, Option<String>), // single type or double type
+    stats: Stats,
+    abilities: (String, Option<String>, Option<String>), // at least 1 ability, max 3
+    gender: Option<(f64, f64)>, // male/female ratio or genderless
+}
+
+pub struct Pokedex {
+    index: Vec<Species>,
 }
 
 
-pub fn file_to_json(path: &str) -> Result<> {
+pub fn read_pokedex(path: &str) -> Result<> {
     Serialize::serialize(&self, serializer)
 }
